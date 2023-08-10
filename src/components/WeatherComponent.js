273570f14ce1e655a79e4e30adc46dd547b11791
DESKTOP-AOHKLAB\Haram <spkim8901@gmail.com>
2023-08-09 23:29:45 -0700

    modified mobile browser

--- src/components/WeatherComponent.js
+++ src/components/WeatherComponent.js
@@ -49,17 +49,28 @@ function WeatherComponent() {
     };
     const cities = Object.keys(MAIN_CITIES).map(city => ({ value: city, label: city }));
     return (
-        <div className="min-h-screen flex justify-center bg-gray-200 pt-20 pb-20 f">
-            <div className="p-8 bg-white shadow-lg rounded-lg flex flex-col space-y-4 w-1/2 min-w-600">
+        <div className="min-h-screen flex justify-center bg-gray-200 pt-20 pb-20">
+            <div className="p-8 bg-white shadow-lg rounded-lg flex flex-col space-y-4 md:w-1/2 w-full md:min-w-600">
                 <Label size="large" className="text-shadow-lg-light">Weather Information</Label>
-                <div className="flex space-x-4">
-                    <Dropdown options={cities} onChange={handleCityChange} className="w-1/2" msg="Select City" />
-                    <Dropdown options={options} onChange={handleUnitChange} className="w-1/2" />
+                <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
+                    <Dropdown options={cities} onChange={handleCityChange} className="w-full md:w-1/2" msg="Select City" />
+                    <Dropdown options={options} onChange={handleUnitChange} className="w-full md:w-1/2" />
                 </div>
                 {renderWeather(weatherData, unit, city, isCityChanging)}
-
             </div>
         </div>
+
+        // <div className="min-h-screen flex justify-center bg-gray-200 pt-20 pb-20 f">
+        //     <div className="p-8 bg-white shadow-lg rounded-lg flex flex-col space-y-4 w-1/2 min-w-600">
+        //         <Label size="large" className="text-shadow-lg-light">Weather Information</Label>
+        //         <div className="flex space-x-4">
+        //             <Dropdown options={cities} onChange={handleCityChange} className="w-1/2" msg="Select City" />
+        //             <Dropdown options={options} onChange={handleUnitChange} className="w-1/2" />
+        //         </div>
+        //         {renderWeather(weatherData, unit, city, isCityChanging)}
+
+        //     </div>
+        // </div>
     );
 }
 
